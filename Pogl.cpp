1c973f9da1f183ecbd060cbf9dde3a4f50456153
Andrey Kirpach <andrki@nangokrstudios.jp>
2021-09-27 21:20:41 +0900

    Open Window, Clear to Black

---
+++ Pogl.cpp
@@ -0,0 +1,79 @@
+﻿#include "Pogl.h"
+
+#include <stdio.h>
+#include <gl/glew.h>
+#include <GLFW/glfw3.h>
+
+// Window Dimensions
+const GLint WIDTH = 800, HEIGHT = 600;
+
+int main()
+{
+	// Init GLFW
+	auto status = glfwInit();
+	const char* errorMessage;
+	if(!status)
+	{
+		glfwGetError(&errorMessage);
+		glfwTerminate();
+		std::cerr << errorMessage;
+		return 1;
+	}
+
+	//Setup GLFW window properties
+	//OpenGL Version
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+
+	//Core profile == No backwards compatibility
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	// Allow Forward compatibility
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+	GLFWwindow* mainWindow = glfwCreateWindow(WIDTH, HEIGHT, "Pogl", nullptr, nullptr);
+
+	if(!mainWindow)
+	{
+		glfwGetError(&errorMessage);
+		glfwTerminate();
+		std::cerr << errorMessage;
+		return 1;
+	}
+
+	// Set context for GLEW to use
+	// can have multiple windows and use this to switch between them
+	glfwMakeContextCurrent(mainWindow);
+
+	// Allow modern extension features
+	glewExperimental = GL_TRUE;
+
+	if(glewInit() != GLEW_OK)
+	{
+		std::cerr << "GLEW init error";
+		glfwDestroyWindow(mainWindow);
+		glfwTerminate();
+		return 1;
+	}
+
+	// Get Buffer size information
+	int bufferWidth, bufferHeight;
+	glfwGetFramebufferSize(mainWindow, &bufferWidth, &bufferHeight);
+
+	//Setup viewport size
+	glViewport(0, 0, bufferWidth, bufferHeight);
+
+	// Loop until window closed
+	while(!glfwWindowShouldClose(mainWindow))
+	{
+		// Get & handle user events
+		glfwPollEvents();
+
+		// Clear window
+		glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		glfwSwapBuffers(mainWindow);
+	}
+
+	return 0;
+}
---
+++ Pogl.h
@@ -0,0 +1,6 @@
+﻿// Pogl.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
