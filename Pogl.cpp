823866992cd6c20e382c4ef6b42646c32de5763b
Andrey Kirpach <andrki@nangokrstudios.jp>
2021-10-07 22:49:21 +0900

    From triangle to tetrahedron

--- Pogl.cpp
+++ Pogl.cpp
@@ -12,29 +12,29 @@
 // Window Dimensions
 constexpr GLint window_width = 800, window_height = 800;
 
-GLuint vao, vbo, shader;
+GLuint vao, ibo, shader;
+GLuint vbo[2];
 GLint uniform_model_matrix;
+
 constexpr float tau = 6.28318531f;
 constexpr float to_radians = tau / 360.f;
 
-
-constexpr float tri_offset_max = 0.7f;
-constexpr float tri_offset_increment = 0.005f;
-float tri_offset = 0.f;
-float direction = 1.f;
+constexpr float tri_angle_increment = 0.005f;
 
 float current_angle = 0.f;
 float current_scale = 1.f;
-
-
 
 // Vertex Shader:
 static const char* vertex_shader =                       "\n\
 #version 330                                             \n\
 layout (location = 0) in vec3 pos;                       \n\
+layout (location = 1) in vec4 col;                       \n\
+\n\
+out vec4 vertexColor;\n\
 uniform mat4 model_matrix; \n\
 void main(){											 \n\
     gl_Position = model_matrix * vec4(pos.xyz, 1.0f);      \n\
+	vertexColor = col;\n\
 }                                                        \n\
 ";
 
@@ -42,34 +42,69 @@ void main(){											 \n\
 // Fragment Shader:
 static const char* fragment_shader =                     "\n\
 #version 330                                             \n\
+in vec4 vertexColor;\n\
 out vec4 color;					                         \n\
 void main(){                                             \n\
-    color = vec4(1.0f, 0.7f, 0.8f, 1.0f);                \n\
+    color = vertexColor;                \n\
 }                                                        \n\
 ";
 
 void create_triangle()
 {
-	GLfloat vertices[] = {
-		-1.0f, 1.0f, 0.0f,
-		1.0f, 1.0f, 0.0f,
-		0.0f, -1.0f, 0.0f,
+	glm::uint indicies[] = {
+		0,3,1,
+		1,3,2,
+		2,3,0,
+		0,1,2
+	};
+
+	auto cos_theta = cos(120.f * to_radians);
+	auto sin_theta = sin(120.f * to_radians);
+
+	GLfloat vertices[] =
+	{
+		1.0f,0.f,0.f,
+		cos_theta, 0,sin_theta,
+		cos_theta, 0,-sin_theta,
+		0.f,sqrt(2.f),0.f
+	};
+
+	GLfloat colors[] = 
+	{
+		1.0f,0.f,0.f,1.f,
+		0.0f,1.f,0.f,1.f,
+		0.0f,0.f,1.f,1.f,
+		1.0f,1.f,0.f,1.f
 	};
 
 	glGenVertexArrays(1, &vao);
 	glBindVertexArray(vao);
 	// from now on, any code using vertex arrays will use the one who's id is vao;
-	glGenBuffers(1, &vbo);
-	glBindBuffer(GL_ARRAY_BUFFER, vbo);
-
+	glGenBuffers(2, vbo);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
 	{
 		glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
 		glEnableVertexAttribArray(0);
 	}
 
+	glBindBuffer(GL_ARRAY_BUFFER, vbo[1]);
+	{
+		glBufferData(GL_ARRAY_BUFFER, sizeof(colors), colors, GL_STATIC_DRAW);
+		glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, 0, nullptr);
+		glEnableVertexAttribArray(1);
+
+	}
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+	glGenBuffers(1, &ibo);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
+	{
+		glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indicies), indicies, GL_STATIC_DRAW);
+	}
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+
 	glBindVertexArray(0);
 }
 
@@ -181,6 +216,8 @@ int main()
 	int buffer_width, buffer_height;
 	glfwGetFramebufferSize(main_window, &buffer_width, &buffer_height);
 
+	glEnable(GL_DEPTH_TEST);
+
 	//Setup viewport size
 	glViewport(0, 0, buffer_width, buffer_height);
 
@@ -204,38 +241,28 @@ int main()
 			current_scale -= 0.1f;
 		}
 
-		tri_offset += tri_offset_increment * direction;
-		if(abs(tri_offset) >= tri_offset_max)
-		{
-			direction *= -1;
-		}
-
 		model_matrix = glm::mat4(1.0f);
 
-		current_angle += tri_offset_increment * 100;
+		current_angle += tri_angle_increment * 100;
 		if(current_angle >= 360)
 		{
 			current_angle -= 360;
 		}
-
 		model_matrix = glm::scale(model_matrix, glm::vec3(current_scale, current_scale, current_scale));
-		model_matrix = glm::translate(model_matrix, glm::vec3(tri_offset, 0.f, 0.f));
-		model_matrix = glm::rotate(model_matrix, current_angle * to_radians, glm::vec3(0, 0, 1));
-
-		//
-
-		//
+		model_matrix = glm::rotate(model_matrix, current_angle * to_radians, glm::vec3(0, 1, 0));
 
 		// Clear window
 		glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
-		glClear(GL_COLOR_BUFFER_BIT);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 		glUseProgram(shader);
 		glUniformMatrix4fv(uniform_model_matrix, 1, GL_FALSE, glm::value_ptr(model_matrix));
 		glBindVertexArray(vao);
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
 
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glDrawElements(GL_TRIANGLES, 12, GL_UNSIGNED_INT, 0);
 
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 		glBindVertexArray(0);
 		glUseProgram(0);
 
