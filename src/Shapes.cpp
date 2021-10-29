df868cde918a511d248b80aaff6c9b4044fc92ea
Andrey Kirpach <andrki@nangokrstudios.jp>
2021-10-29 15:39:30 +0900

    Fix tetrahedron triangle winding order

--- src/Shapes.cpp
+++ src/Shapes.cpp
@@ -37,10 +37,10 @@ Mesh* create_tetrahedron()
 	};
 
 	glm::uint indices[] = {
-		0,3,1,
-		2,1,3,
-		2,3,0,
-		0,1,2
+		0,1,3,
+		1,2,3,
+		2,0,3,
+		0,2,1
 	};
 
 	glm::vec3* normals = new glm::vec3[vertex_count];
